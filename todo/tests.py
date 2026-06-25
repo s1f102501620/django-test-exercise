7af820446e3a723b6731650b62cc6a1ee10bfa83
1F10250162 <s1f102501620@iniad.org>
2026-06-25 16:52:45 +0900

    flake8のエラーを修正１

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -26,13 +28,14 @@ class TaskModelTestCase(TestCase):
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, "task2")
         self.assertFalse(task.completed)
-        self.assertIsNone(task.due_at, None)
+        self.assertIsNone(task.due_at)
 
-class TaskModelTestCase(TestCase):
+
+class TaskModelOverdueTestCase(TestCase):
 
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        
+
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title="task1", due_at=due)
         task.save()
@@ -49,17 +52,17 @@ class TaskModelTestCase(TestCase):
 
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
-        task = Task(title="task3",due_at=None)
+        task = Task(title="task3", due_at=None)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
 
 class TodoViewTestCase(TestCase):
     def test_index_view(self):
         client = Client()
         response = client.get("/")
-        
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(len(response.context["tasks"]), 0)
@@ -98,4 +101,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, "todo/index.html")
         self.assertEqual(response.context["tasks"][0].title, "task1")
         self.assertEqual(response.context["tasks"][1].title, "task2")
-    
--- todo/views.py
+++ todo/views.py
@@ -3,19 +3,21 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
+
 # Create your views here.
 def index(request):
     if request.method == "POST":
         task = Task(title=request.POST["title"],
                     due_at=make_aware(parse_datetime(request.POST["due_at"])))
         task.save()
-    
+
     if request.GET.get("order") == "due":
         tasks = Task.objects.order_by("due_at")
     else:
         tasks = Task.objects.order_by("-posted_at")
 
     context = {
-        "tasks": tasks}
-    
+        "tasks": tasks
+    }
+
     return render(request, "todo/index.html", context)
